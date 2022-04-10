b2f4b5c4210ddcdf02a5d06a6967aeb3afba76c7
juanpa6lo <juanpablo.contr3ras@gmail.com>
2022-04-10 09:53:12 -0500

    formatting

--- deploy/00_deploy_erc20.js
+++ deploy/00_deploy_erc20.js
@@ -3,6 +3,7 @@ const { ethers } = require("hardhat");
 async function main() 
 {
     const [deployer] = await ethers.getSigners();
+
     console.log
     (
         "Deploying contracts with the account:", deployer.address
@@ -20,7 +21,7 @@ async function main()
 main()
 .then(() => process.exit(0))
 .catch
-(error => 
+(error =>
 {
   console.error(error);
   process.exit(1);
--- hardhat.config.js
+++ hardhat.config.js
@@ -23,10 +23,13 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY 
 const REPORT_GAS = process.env.REPORT_GAS 
 
-module.exports = {
+module.exports = 
+{
   defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
+  networks: 
+  {
+    hardhat: 
+    {
       // If you want to do some forking set `enabled` to true
       //forking: {
         //url: ALCHEMY_MAINNET_RPC_URL,
@@ -35,10 +38,12 @@ module.exports = {
       //},
       //chainId: 31337,
     },
-    localhost: {
+    localhost: 
+    {
       chainId: 31337,
     },
-    kovan: {
+    kovan: 
+    {
       url: KOVAN_RPC_URL,
       accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
       //accounts: {
@@ -47,7 +52,8 @@ module.exports = {
       saveDeployments: true,
       chainId: 42,
     },
-    rinkeby: {
+    rinkeby: 
+    {
       url: RINKEBY_RPC_URL,
       accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
       //   accounts: {
@@ -56,7 +62,8 @@ module.exports = {
       saveDeployments: true,
       chainId: 4,
     },
-    mumbai: {
+    mumbai: 
+    {
       url: MUMBAI_RPC_URL,
       accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
       //   accounts: {
@@ -66,41 +73,51 @@ module.exports = {
       chainId: 80001,
     },
   },
-  etherscan: {
+  etherscan: 
+  {
     // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
-    apiKey: {
+    apiKey: 
+    {
       rinkeby: ETHERSCAN_API_KEY,
       kovan: ETHERSCAN_API_KEY,
       polygon: POLYGONSCAN_API_KEY,
     },
   },
-  gasReporter: {
+  gasReporter: 
+  {
     enabled: REPORT_GAS,
     currency: "USD",
     outputFile: "gas-report.txt",
     noColors: true,
     // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
   },
-  namedAccounts: {
-    deployer: {
+  namedAccounts: 
+  {
+    deployer: 
+    {
       default: 0, // here this will by default take the first account as deployer
       1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
     },
-    feeCollector: {
+    feeCollector: 
+    {
       default: 1,
     },
   },
-  solidity: {
-    compilers: [
+  solidity: 
+  {
+    compilers: 
+    [
       {
         version: "0.8.7",
       },
     ],
   },
-  mocha: {
+  mocha: 
+  {
     timeout: 200000, // 200 seconds max for running tests
   },
-  paths: {
+  paths: 
+  {
   sources: "./contracts",
   tests: "./test",
   cache: "./cache",
