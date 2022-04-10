cadb85e0e96ac627c61e800fb6437717611f8443
juanpa6lo <juanpablo.contr3ras@gmail.com>
2022-04-10 09:40:38 -0500

    added deploy scripts for erc20

---
+++ deploy/00_deploy_erc20.js
@@ -0,0 +1,28 @@
+const { ethers } = require("hardhat");
+
+async function main() 
+{
+    const [deployer] = await ethers.getSigners();
+    console.log
+    (
+        "Deploying contracts with the account:", deployer.address
+    );
+
+    console.log
+    (
+        "Account balance:", (await deployer.getBalance()).toString()
+    );
+
+    const OwaneToken = await ethers.getContractFactory("OwaneToken");
+    const deployedContract = await OwaneToken.deploy();
+    console.log("Deployed ERC20 contract address:", deployedContract.address)
+}
+main()
+.then(() => process.exit(0))
+.catch
+(error => 
+{
+  console.error(error);
+  process.exit(1);
+}
+);
